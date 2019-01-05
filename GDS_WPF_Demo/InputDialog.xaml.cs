06c15e3967f62d99121df82a183808023435e213
Duck <tiger.duck1998@gmail.com>
2019-01-05 15:23:05 +0800

    Add universal input dialog

---
+++ GDS_WPF_Demo/InputDialog.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GDS_WPF_Demo
+{
+    /// <summary>
+    /// InputDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class InputDialog : Window
+    {
+        public InputDialog(string windowtitle)
+        {
+            InitializeComponent();
+            Title = windowtitle;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            Answer.Focus();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        public string ReturnGDName
+        {
+            get
+            {
+                return Answer.Text;
+            }
+        }
+    }
+}
